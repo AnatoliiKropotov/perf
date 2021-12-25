801305ec2af69c8bc8148dabb4581efdba4d4706
AnatoliiKropotov <96647156+AnatoliiKropotov@users.noreply.github.com>
2021-12-25 15:07:09 +0300

    Update task3.py

--- task3/task3.py
+++ task3/task3.py
@@ -2,37 +2,61 @@ import json
 
 fhand_tests = input('Введите имя файла, макет:')
 fhand_values = input('Введите имя файла, значения:')
-fh_tests = open(fhand_tests)           
+fh_tests = open(fhand_tests)
 fh_values = open(fhand_values)
-fdata_tests = fh_tests.read()          
+fdata_tests = fh_tests.read()
 fdata_values = fh_values.read()
 data_tests = json.loads(fdata_tests)
-data_values = json.loads(fdata_values) 
+data_values = json.loads(fdata_values)
 
 
+
+def func4(id, value, list):
+    print('переход в func4')
+    for i in range(0,len(list)):
+        if list[i]['id'] == id:
+            list[i]['value'] = value
+            print('выход из func4')
+            return
+
+def func3(id, value, list):
+    print('переход в func3')
+    for i in range(0,len(list)):
+        if list[i]['id'] == id:
+            list[i]['value'] = value
+            print('выход из func3')
+            return
+    for i in range(0,len(list)):
+        if list[i].get('values') != None:
+            func4(id, value, list = list[i]['values'])
+            return
+
+def func2(id, value, list):
+    print('переход в func2')
+    for i in range(0,len(list)):
+        if list[i]['id'] == id:
+            list[i]['value'] = value
+            print('выход из func2')
+            return True
+        elif list[i].get('values') != None:
+            if func3(id, value, list = list[i]['values']) == True:
+                return
+
+def func(id, value, list):
+    for a in range(0,len(list)):
+        if list[a]['id'] == id:
+            list[a]['value'] = value
+            return
+        elif list[a].get('values') != None:
+            if func2(id, value, list[a]['values']) == True:
+                return
 # здесь должна быть рекурсивная функция,к этому я пришёл, но написать её не успел :(
 r = open('report.json', 'w')
 for i in range(0, len(data_values['values'])):
+    print('start')
     id = data_values["values"][i]['id']
     value = data_values["values"][i]['value']
-    for a in range(0,len(data_tests['tests'])):
-        if data_tests['tests'][a]['id'] == id:
-            data_tests['tests'][a]['value'] = value
-            break
-        elif data_tests['tests'][a].get('values') != None:
-            for b in range(0, len(data_tests['tests'][a]['values'])):
-                if data_tests['tests'][a]['values'][b]['id'] == id:
-                    data_tests['tests'][a]['values'][b]['value'] = value
-                    break
-                elif data_tests['tests'][a]['values'][b].get('values') != None:
-                    for c in range(0, len(data_tests['tests'][a]['values'][b]['values'])):
-                        if data_tests['tests'][a]['values'][b]['values'][c]['id'] == id:
-                            data_tests['tests'][a]['values'][b]['values'][c]['value'] = value
-                            break
-                        elif data_tests['tests'][a]['values'][b]['values'][c].get('values') != None:
-                            for d in range(0, len(data_tests['tests'][a]['values'][b]['values'][c]['values'])):
-                                if data_tests['tests'][a]['values'][b]['values'][c]['values'][d]['id'] == id:
-                                    data_tests['tests'][a]['values'][b]['values'][c]['values'][d]['value'] = value
-                                    break
+    list = data_tests['tests']
+    func(id, value, list)
 
 json.dump(data_tests, r, sort_keys=True, indent=2)
