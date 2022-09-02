8dde011d185b6a1fd4b6e45ccebe6ea55763e069
AnatoliiKropotov <96647156+AnatoliiKropotov@users.noreply.github.com>
2022-09-02 16:41:18 +0300

    Update avk_bot.py
    Replacement of the cryptocurrency exchange from exmo to bitfinex. Added the function of viewing the dollar/euro exchange rate.

--- btc_bot/avk_bot.py
+++ btc_bot/avk_bot.py
@@ -15,21 +15,23 @@ def feedback(message, func):
 
 
 #запуск бота
-@bot.message_handler(commands='start')
+@bot.message_handler(commands=['start'])
 def start(message):
-    graz = open('xxxx/xxxx/graz.webp', 'rb')
+    graz = open('/home/akk1/graz.webp', 'rb')
     bot.send_sticker(message.chat.id, graz)
     bot.send_message(message.chat.id, "Поздравляю, бот запущен!")
+    bot.send_message(message.chat.id, "/help - посмотреть список команд ")
     func = 'start'
     feedback(message, func)
 
 
 #вызов списка команд
-@bot.message_handler(commands='help')
+@bot.message_handler(commands=['help'])
 def help_func(message):
     bot.send_message(message.chat.id, '''
 Cписок команд:
 /crypto - узнать стоимость криптовалюты
+/currency - узнать стоимость валюты
 /my_id - узнать свой id
 /you_awesome - сказать боту что он потрясающий
 /about - об авторе
@@ -39,7 +41,7 @@ Cписок команд:
 
 
 #узнать свой id
-@bot.message_handler(commands='my_id')
+@bot.message_handler(commands=['my_id'])
 def id_func(message):
     bot.send_message(message.chat.id, f'твой id {message.from_user.id}')
     func = 'id_func'
@@ -47,66 +49,158 @@ def id_func(message):
 
 
 #выбор криптовалютной пары
-@bot.message_handler(commands='crypto')
+@bot.message_handler(commands=['crypto'])
 def btc_func(message):
     markup = types.InlineKeyboardMarkup(row_width=2)
-    button1 = types.InlineKeyboardButton('биткоин/доллар', callback_data = 'BTC_USD')
-    button2 = types.InlineKeyboardButton('биткоин/рубль', callback_data = 'BTC_RUB')
-    button3 = types.InlineKeyboardButton('эфир/доллар', callback_data = 'ETH_USD')
-    button4 = types.InlineKeyboardButton('эфир/рубль', callback_data = 'ETH_RUB')
+    button1 = types.InlineKeyboardButton('биткоин/доллар', callback_data = 'BTCUSD')
+    button2 = types.InlineKeyboardButton('биткоин/рубль', callback_data = 'BTCRUB')
+    button3 = types.InlineKeyboardButton('эфир/доллар', callback_data = 'ETHUSD')
+    button4 = types.InlineKeyboardButton('эфир/рубль', callback_data = 'ETHRUB')
     markup.add(button1, button2, button3, button4)
     bot.send_message(message.chat.id, 'выбери криптовалютную пару', reply_markup=markup)
     func = 'crypto'
     feedback(message, func)
 
+#выбор валютной пары
+@bot.message_handler(commands=['currency'])
+def cur_func(message):
+    markup = types.InlineKeyboardMarkup(row_width=2)
+    button1 = types.InlineKeyboardButton('доллар/рубль', callback_data = 'USD')
+    button2 = types.InlineKeyboardButton('евро/рубль', callback_data = 'EUR')
+    markup.add(button1, button2)
+    bot.send_message(message.chat.id, 'выбери валютную пару', reply_markup=markup)
+    func = 'currency'
+    feedback(message, func)
 
-#узнать цену криптовалюты
+#узнать цену, после нажатия на кнопку
 @bot.callback_query_handler(func=lambda call: True)
 def callback_inline(call):
-    url = "https://api.exmo.com/v1.1/ticker"
-    payload={}
-    headers = {'Content-Type': 'application/x-www-form-urlencoded'}
-    response = requests.request("GET", url, headers=headers, data=payload)
-    data = response.text
 
-    try:
-        js = json.loads(data)
-    except:
-        bot.send_message(call.message.chat.id, 'не получилось')
-    rate = int(float((js[call.data]['sell_price'])))
+    # запрос на биржу Bitfinex
+    def pair_r_quest(pair, number):
+            url = "https://api-pub.bitfinex.com/v2/"
+            pathParams = "tickers"
+            queryParams = f"symbols=fUSD,t{pair}"
 
-    if call.data == 'BTC_USD':
-        a = 'биткоин'
-        b = 'долларов'
-    elif call.data == 'BTC_RUB':
-        a = 'биткоин'
-        b = 'рублей'
-    elif call.data == 'ETH_USD':
-        a = 'эфир'
-        b = 'долларов'
-    elif call.data == 'ETH_RUB':
-        a = 'эфир'
-        b = 'рублей'
+            response = requests.request("GET", url + pathParams + "?" + queryParams)
+
+            data = response.text
+            try:
+                js = json.loads(data)
+            except:
+                bot.send_message(call.message.chat.id, 'не получилось')
+
+            rate_pair = int(js[1][1])
+            if number == 1:
+                return rate_pair
+            else:
+                message(rate_pair)
+
+
+    # запрос на сайт www.cbr-xml-daily.ru
+    def currency_r_quest(currency, number):
+        url = "https://www.cbr-xml-daily.ru/daily_json.js"
+        x = requests.get(url)
+        data = x.text
+        try:
+            js = json.loads(data)
+        except:
+            bot.send_message(call.message.chat.id, 'не получилось')
+
+        current_value = str(js["Valute"][currency]['Value'])
+        current_rate = float(current_value[0:4])
+        if number == 1:
+            return current_rate
+        else:
+            message(current_rate)
+
+
+
+    #считаем стоимость в рублях
+    def rub_func(pair, currency, number):
+        rate = int(float(pair_r_quest(pair, number) * currency_r_quest(currency,number)))
+        message(rate)
+
+
+
+    # вывод сообщения с ценой
+    def message(rate):
+        bot.send_message(call.message.chat.id, f'1 {a} стоит {rate} {b}')
+        bot.delete_message(chat_id=call.message.chat.id, message_id=call.message.message_id)
+
+
+
+    # распределяем кнопки
+    number = 0
+    if call.data == 'BTCUSD' or call.data == 'ETHUSD':
+        if call.data == 'BTCUSD':
+            a = 'биткоин'
+            b = 'долларов'
+            pair = call.data
+            pair_r_quest(pair, number)
+        elif call.data == 'ETHUSD':
+            a = 'эфир'
+            b = 'долларов'
+            pair = call.data
+            pair_r_quest(pair, number)
+        else:
+            bot.send_message(call.message.chat.id, 'Что-то пошло не так')
+
+    elif call.data == 'USD' or call.data == 'EUR' :
+        if call.data == 'USD':
+            a = 'доллар'
+            b = 'рублей'
+            currency = call.data
+            currency_r_quest(currency, number)
+
+        elif call.data == 'EUR':
+            a = 'евро'
+            b = 'рублей'
+            currency = call.data
+            currency_r_quest(currency, number)
+
+        else:
+            bot.send_message(call.message.chat.id, 'Что-то пошло не так')
+
+    elif call.data == 'BTCRUB' or call.data == 'ETHRUB':
+        number = 1
+        if call.data == 'BTCRUB':
+            a = 'биткоин'
+            b = 'рублей'
+            pair = "BTCUSD"
+            currency = "USD"
+            rub_func(pair, currency, number)
+        elif call.data == 'ETHRUB':
+            a = 'эфир'
+            b = 'рублей'
+            pair = "ETHUSD"
+            currency = "USD"
+            rub_func(pair, currency, number)
+        else:
+            bot.send_message(call.message.chat.id, 'Что-то пошло не так')
+
+
     else:
-        bot.send_message(call.message.chat.id, 'Что-то пошло не так')
-    bot.send_message(call.message.chat.id, f'1 {a} стоит {rate} {b}')
-    bot.delete_message( chat_id=call.message.chat.id, message_id=call.message.message_id)
+        bot.send_message(call.message.chat.id, 'не получилось')
+
+
 
 
 #информация о создателе
-@bot.message_handler(commands='about')
+@bot.message_handler(commands=['about'])
 def about_func(message):
-    bot.send_message(message.chat.id, '''
-Анатолий Кропотов
-id 491353015
+    bot.send_message(message.chat.id, f'''
+Aнатолий Кропотов
+@{message.from_user.username}
 ak0391@yandex.ru''')
     func = 'about_func'
     feedback(message, func)
 
+
 #ты потрясающий
-@bot.message_handler(commands='you_awesome')
+@bot.message_handler(commands=['you_awesome'])
 def awesome_func(message):
-    keanu = open('xxxx/xxxx/keanu.webp', 'rb')
+    keanu = open('/home/akk1/keanu.webp', 'rb')
     bot.send_sticker(message.chat.id, keanu)
     if message.from_user.username != None:
         bot.send_message(message.chat.id, f'нет, {message.from_user.username}, это ты потрясающий!')
